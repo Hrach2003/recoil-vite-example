51ce65089c937de484c930b30fa83f52a6f48d4f
Hrach2003 <hrachya.hovakimyan.03@mail.ru>
2021-05-10 23:03:22 +0400

    feat(Stats): optimize rerender

--- src/components/Todo/TodoStats.tsx
+++ src/components/Todo/TodoStats.tsx
@@ -4,21 +4,32 @@ import { todoStatisticSelector } from "../../stores/todo.module";
 import { Button } from "../Button";
 import { LightText } from "../LightText";
 
+const CompletedTotalRatio = () => {
+  const { completed, total } = useRecoilValue(todoStatisticSelector);
+  return (
+    <Button disabled>
+      {completed}/{total}
+    </Button>
+  );
+};
+
+const PersentCompleted = () => {
+  const { persent } = useRecoilValue(todoStatisticSelector);
+  return <Button disabled>{persent}%</Button>;
+};
+
 export const TodoStats = () => {
-  const { completed, persent, total } = useRecoilValue(todoStatisticSelector);
   return (
     <div className="px-5 py-2 space-y-2">
       <h1 className="text-2xl text-center mb-3">Todo Stats</h1>
       <div>
         <p>
           <LightText>number of completed todos: </LightText>
-          <Button disabled>
-            {completed}/{total}
-          </Button>
+          <CompletedTotalRatio />
         </p>
         <p>
           <LightText>completed: </LightText>
-          <Button disabled>{persent}%</Button>
+          <PersentCompleted />
         </p>
       </div>
     </div>
